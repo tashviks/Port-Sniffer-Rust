71bb7454aadcf4aa070dd902164c1ea9403ca12a
Tashvik Srivastava <tashvik.12112279@lpu.in>
2024-05-30 04:23:59 +0530

    added necessary code - now functional

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use std::{env, net::IpAddr , str::FromStr};
+use std::io::Write;
+use std::net::TcpStream;
+use std::{env, net::IpAddr, process, str::FromStr};
+use std::sync::mpsc::{channel, Sender};
+use std::{io, thread};
 
 struct Arguments{
     flag : String,
     ipadr : IpAddr,
     threads : u16,
 }
+
+const MAX : u16 = 65535;
 
 impl Arguments {
     fn new(args : &[String]) -> Result<Arguments , &'static str> {
@@ -44,7 +50,7 @@ impl Arguments {
                 let threads = match args[2].parse::<u16>(){
                     Ok(s)=> s,
                     Err(_) => return Err("Invalid Thread Number"),
-                }
+                };
                 return Ok(Arguments{
                     threads,
                     flag,
@@ -61,8 +67,60 @@ impl Arguments {
 
     }
 }
+
+fn scan(tx: Sender<u16> , start_port : u16 , addr : IpAddr , num_threads:u16){
+    let mut port : u16 = start_port+1;
+    loop{
+        match TcpStream::connect((addr , port)){
+            Ok(_)=>{
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_)=>{}
+        }
+        if(MAX - port) <= num_threads {
+            break;
+        }
+        port += num_threads;
+    }
+}
 fn main() {
     let args : Vec<String> = env::args().collect();
     let program = args[0].clone();
+    let arguments = Arguments::new(&args).unwrap_or_else(
+        |err|{
+             if err.contains("help") {
+                process::exit(0);
+            }
+            else{
+                eprintln!("{} problem parsing arguments : {}" , program , err);
+                process::exit(0);
+            }
+        }
+    );
 
+    let num_threads = arguments.threads;
+    let addr  = arguments.ipadr;
+    let (tx , rx) = channel();
+    for i in 0..num_threads {
+        let tx = tx.clone();
+        thread::spawn(move || {
+            scan(tx , i , addr , num_threads);
+        });
+    }
+
+    let mut out = vec![];
+    drop(tx);
+
+    for p in rx {
+        out.push(p);
+    }
+
+    println!("");
+    out.sort();
+
+    for v in out {
+        println!("{} is open" , v);
+    }
 }
